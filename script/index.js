941c89b209cda5577b5204c5c252d400bd0e05e1
Ferdousnayeem <mostafanayeem11@gmail.com>
2026-02-19 22:51:36 +0600

    top rated products

---
+++ script/index.js
@@ -0,0 +1,54 @@
+const loadTopRatedProducts = () => {
+    fetch('https://fakestoreapi.com/products')
+    .then (response => response.json())
+    .then(data => displayTopRatedProducts(data))
+}
+
+
+const displayTopRatedProducts = (products) => {
+
+    // console.log(products);
+    const topRatedProducts = products.filter(product => product.rating && product.rating.rate >= 3)
+    // console.log(topRatedProducts);
+    const firstThreeProducts = topRatedProducts.slice(0, 3)
+    console.log(firstThreeProducts);
+
+    const topRatedContainer = document.getElementById('trending-product-container')
+    topRatedContainer.innerHTML = ""
+
+    firstThreeProducts.forEach(product => {
+      const createTopRatedProducts = document.createElement('div')
+      
+      createTopRatedProducts.innerHTML = `
+              <div class="card bg-base-100 w-96 h-full shadow-sm">
+                <figure>
+                  <img class="px-20 py-10 bg-gray-200 w-full h-96"
+                    src=${product.image}
+                    alt="" />
+                </figure>
+                <div class="card-body">
+                  <div class="flex justify-between">
+                    <div class="badge badge-soft badge-primary">${product.category}</div>
+                    <div class="">
+                      <p class=""><i class="fa-regular fa-star"></i> ${product.rating.rate} (${product.rating.count})</p>
+                    </div>
+                  </div>
+                  <h2 class="card-title">
+                  ${product.title}
+                  </h2>
+                  <p>${product.description.slice(0, 100)}...</p>
+                  <div class="card-actions flex justify-between">
+                  <div class="btn btn-outline px-6"><i class="fa-regular fa-eye pr-6"></i> Details</div>
+                  <div class="btn btn-primary px-10"><i class="fa-solid fa-cart-shopping pr-6"></i> Add</div>
+                  </div>
+                </div>
+              </div>
+                `
+                topRatedContainer.appendChild(createTopRatedProducts)
+    });
+
+    
+
+}
+
+loadTopRatedProducts();
